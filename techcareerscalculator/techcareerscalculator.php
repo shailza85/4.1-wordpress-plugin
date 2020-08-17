4b822b94afa8dbca19b0d937264bdc17852f0bac
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-08-17 14:05:24 -0600

    Creating new plugin techcareerscalculator

---
+++ techcareerscalculator/techcareerscalculator.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Tech Careers Calculator
+Plugin URI: http://techcareers.ca/
+Description: An example plugin for techcareers class. It provides an example of a wordpress shortcode.
+//Type [techcareerscalculator] into a page ot post to output our calculator.
+Author: Shailza Sharma
+Version: 1.7.2
+Author URI: http://techcareers.ca/
+*/
